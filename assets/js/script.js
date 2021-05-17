c62d42b3a71c6dab9d0dd14f4435fafdf8cc92d9
CaioBertolini <caio.bertolini58@gmail.com>
2021-05-17 13:00:29 -0300

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+// Animação de scrool quando clica no link
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
